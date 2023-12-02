9010987813cff13e0bebfb68562eed4ef289fe7b
a_fergus <45887642+aferg439@users.noreply.github.com>
2023-12-02 06:52:01 -0500

    Initial case works

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,32 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
+    let string_to_mock = &args.string;
+    let result: String = mock(&string_to_mock);
 
-    let content = std::io::read_to_string(&args.string).expect("DiD yOu MeAn To NoT MoCk SoMeThInG? (You didn't provide a string)");
-    let mut new_vec: Vec<char>;
+    println!("{}", result)
+}
 
+fn mock(s: &str) -> String {
     let mut count: u32 = 0;
+    let mut result: String = String::new();
 
-    // Loop over the vector chars, if not alphabetic then skip, otherwise increment count by 1
-    // If count is divisible by two then capitalize the alphabetic character
-    for c in content.to_lowercase().chars() {
+    // iterate over characters, alternating upper and lower case letters and skipping
+    // non-alphabetic characters
+    for c in s.to_lowercase().chars() {
         if !c.is_alphabetic() {
-
+            result.push(c);
+        } else {
+            if count % 2 == 0 {
+                let new_string: String = c.to_string().to_uppercase();
+                result.push(new_string.chars().next().expect("String is empty"));
+                count += 1;
+            } else {
+                let new_string: String = c.to_string().to_lowercase();
+                result.push(new_string.chars().next().expect("String is empty."));
+                count += 1;
+            }
         }
     }
+    result
 }
