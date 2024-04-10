3bcd7d29d94445c68df274e26ffb5a853563b41d
a_fergus <45887642+aferg439@users.noreply.github.com>
2024-04-10 16:23:07 -0400

    most recent changes
    some changes idk

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,50 @@ struct Cli {
     // A statement you'd like to convert to mockCase (i.e. SoMeThInG LiKe ThIs)
     #[arg(short, long)]
     string: String,
+
+    // An ASCII art depiction of SpongeBob mocking your target
+    // Argument is optional, if not present, the string will be mocked and returned
+    #[arg(short, long)]
+    picture: bool,
+
+    // An optional argument to call a Generative AI model to mock the string
+    #[arg(short, long)]
+    generate: Model,
+}
+
+// Provide options and configs for different model providers
+struct Model {
+    // The name of the model
+    model: String,
+
+    // The model's API key - pulled from environment variables
+    api_key: String,
+
+    // The model's endpoint - implicitly defined by the function call
+    endpoint: String,
 }
 
 fn main() {
     let args = Cli::parse();
-    let string_to_mock = &args.string;
-    let result: String = mock(&string_to_mock);
 
-    println!("{}", result)
+    // Access string for mocking
+    let string_to_mock = &args.string;
+
+    // if bedrock arg present, call Amazon Bedrock to mock the string
+    // TODO implement a case for different model providers
+    if args.bedrock {
+        let result: String = mock_with_bedrock(&string_to_mock);
+    } else {
+        let result: String = mock(&string_to_mock);
+    }
+
+    // If picture arg present, draw the meme - otherwise return mocked string.
+    if args.picture {
+        draw_meme();
+        println!("{}", result);
+    } else {
+        println!("{}", result);
+    }
 }
 
 fn mock(s: &str) -> String {
@@ -39,4 +75,32 @@ fn mock(s: &str) -> String {
         }
     }
     result
+}
+
+fn draw_meme() {
+    // draw mocking spongebob meme if arg present
+    println!(r"⠀⠀⠀⠀⠀⠀⠀⠀⣠⣶⣖⡲⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡏⠀⠀⠑⣄⢣⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢧⠀⣄⠀⠈⣆⢣⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⠹⡆⠀⠈⢧⢧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣠⠤⢤⣨⣧⡾⠦⠃⠀⢳⣷⠦⣤⣤⡖⠒⠒⠲⢴⣶⣯⣝⠒⠒⢋⡹⠳⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣠⣤⠞⠁⢻⡿⡿⠿⠿⢤⣀⣤⣾⣿⣦⠈⣿⣿⣦⣤⣤⡀⠉⠙⠉⠀⡰⠋⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⣀⣤⡞⠁⠀⢀⣀⡀⠿⠿⢆⣠⡼⠟⠋⢉⣾⠏⠀⠛⢻⣿⣛⡛⠁⠀⣀⡤⠚⠀⠀⣀⡀⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⣟⠀⣽⣭⡍⠉⠉⠉⠉⠱⣿⣁⣀⣠⣴⣿⠋⠀⠉⠉⠉⢀⣴⣮⡉⠉⠁⢹⠀⢠⣾⣿⣿⡌⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠈⠑⣭⠝⣿⠀⢠⠀⠀⠀⠈⠛⠛⠛⠉⠀⠀⠀⠀⡴⠊⣿⣀⣸⡉⠑⠂⢸⠀⢸⣿⣿⡿⠃⣏⠀⠀⠀⠀⠀⠀⠀⣠⡴⠻⡆⠀⠀⠀
+⢠⣞⡿⠚⠋⠉⠙⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠟⠊⠉⠉⠉⠙⠲⡄⠈⢆⠀⠉⠉⢀⣀⠘⢦⠀⢀⡤⠤⣤⡞⣩⣶⢠⠇⠀⠀⠀
+⠀⡟⣿⣾⡓⣆⠀⠀⠘⡆⠀⠀⠀⠀⠀⠀⠀⢠⡏⠀⠀⠀⣰⣶⣖⣆⢹⠀⠀⣳⡀⠀⢺⡿⠂⢸⡶⠋⠀⠀⠀⢹⠁⡇⢸⢠⣶⢶⣄
+⠀⢧⡙⠿⠟⠃⠀⠀⡰⠃⡀⠀⠀⠀⠀⠀⠀⠈⢧⡀⠀⠀⠘⠿⣿⣿⠏⠀⠀⢸⡇⠀⠀⠀⢠⣾⠁⠀⠀⠀⣠⠞⢰⡇⣸⣿⢏⡏⣽
+⠀⠈⠙⢦⣤⣤⠴⠊⣡⠞⢡⡟⠀⠀⠀⠀⠀⠀⠀⠙⣖⣒⡒⣚⣩⡵⠂⠀⠀⠀⠳⣄⠀⣰⣿⣇⠀⢀⣠⣶⣿⣶⣿⡇⢟⡤⠘⣰⠃
+⠀⠀⠀⠀⣨⠷⠖⠋⠁⢀⡞⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⠘⡆⢹⣿⡿⠋⠉⡀⠙⣿⠛⢻⣧⣌⣤⣾⡇⠀
+⠀⠀⠀⠸⣇⠀⠀⣀⡴⠋⠀⠿⠁⠀⠀⠀⢀⣀⣤⣤⣤⣤⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠧⣀⠉⠀⠀⢾⣿⠀⣯⠀⠈⣿⣿⣿⣿⣿⡄
+⠀⠀⠀⠀⠈⠉⢹⠏⠀⠀⠀⠀⠀⢀⣠⣶⣿⣿⡿⠿⠛⠛⠋⣁⡀⠀⠀⣴⣶⣶⣶⣤⠀⠀⢱⡀⠀⠀⠀⠀⢸⠀⠀⢸⣿⣿⡿⠟⠋
+⠀⠀⠀⠀⠀⠀⢸⡄⠀⠀⠀⠀⣰⣟⡿⡟⢋⣁⣤⠤⠶⠛⠛⠉⠀⠀⠀⠛⠛⠛⠛⢉⣠⣄⡀⠳⠶⢤⡀⠀⠸⡆⠀⣠⠿⠋⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢧⠀⠀⠀⡾⠙⠧⠔⠚⠛⠳⠤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠋⠁⠀⢀⣀⣉⣭⡷⠟⠉⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⢧⡀⡼⠁⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⡾⠛⠒⠒⠒⠒⠒⠚⠛⢻⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠁⠀⠀⠀⠀⠀⠀⢀⡸⢋⡽⠿⠿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⠇⣿⠿⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣴⣿⡦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⢰⣇⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣷⣦⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣏⠓⢿⣿⣦⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢿⣿⣿⠿⠿⠿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣤⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀");
 }
